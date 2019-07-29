9061e73f33f121ddaf3efdbe8355b3571aaa4111
AnastasiyaBH <bhanastasia495@gmail.com>
2019-07-29 18:40:56 +0300

    feat: add searchBox markup

---
+++ src/components/searchBox/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Button from '@material-ui/core/Button';
+import Input from '@material-ui/core/Input';
+
+const SearchBox = () => (
+  <div>
+    <Input>
+     Search
+    </Input>
+    <Button>
+      Search
+    </Button>
+  </div>
+);
+
+export default SearchBox;
