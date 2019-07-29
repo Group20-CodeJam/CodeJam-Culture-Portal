7f847f66a1592200985f6c11bc103e1aac231f3f
AnastasiyaBH <bhanastasia495@gmail.com>
2019-07-29 18:59:28 +0300

    feat: apply styles.js

--- src/components/searchBox/index.js
+++ src/components/searchBox/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import Button from '@material-ui/core/Button';
 import Input from '@material-ui/core/Input';
+import { withStyles } from '@material-ui/styles';
 
-const SearchBox = () => (
-  <div>
-    <Input>
-     Search
-    </Input>
-    <Button>
+import styles from './styles';
+
+
+const SearchBox = (props) => {
+  const { classes } = props;
+  return (
+    <div>
+      <Input>
       Search
-    </Button>
-  </div>
-);
+      </Input>
+      <Button className={classes.button}>
+        Search
+      </Button>
+    </div>
+  );
+};
 
-export default SearchBox;
+SearchBox.propTypes = {
+  classes: PropTypes.objectOf(PropTypes.object).isRequired,
+};
+
+export default withStyles(styles)(SearchBox);
---
+++ src/components/searchBox/styles.js
@@ -0,0 +1,5 @@
+export default {
+  button: {
+    background: '#744b67',
+  },
+};
