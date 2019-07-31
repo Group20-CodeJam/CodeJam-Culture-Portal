2fdcfdfb28f45dbefc8dc1fcb0c9ce9132465ab1
Vadzim Minin <vadzim.minin@gmail.com>
2019-07-31 12:02:51 +0300

    feat: add style for description on page

---
+++ src/components/description/styles.js
@@ -0,0 +1,16 @@
+export default {
+  paragraph: {
+    textIndent: '20px',
+    fontFamily: [
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
+};
