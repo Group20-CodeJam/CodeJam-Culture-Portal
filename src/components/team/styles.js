49a10010ef4de2f0cc3a2044b0482a7d2da3bbeb
Vadzim Minin <vadzim.minin@gmail.com>
2019-07-31 12:04:35 +0300

    feat: add style for team page

---
+++ src/components/team/styles.js
@@ -0,0 +1,32 @@
+export default {
+  teamContainer: {
+    textAlign: 'center',
+    fontSize: '0.8em',
+    height: '360px',
+    width: '164px',
+    boxShadow: '0 0 20px rgba(0, 0, 0, 0.26)',
+    display: 'inline-block',
+    margin: '9px',
+    transition: '0.2s ease box-shadow',
+    overflow: 'hidden',
+    borderRadius: '15px',
+  },
+  teamNickname: {
+    color: '#744b67',
+    fontWeight: 'bold',
+  },
+  teamWrapper: {
+    textAlign: 'center',
+  },
+  icon: {
+    width: '26px',
+    height: '26px',
+  },
+  teamIcon: {
+    height: '164px',
+  },
+  linkGit: {
+    fontSize: '40px',
+    color: '#3d282e',
+  },
+};
