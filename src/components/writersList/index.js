0a18021a46b87178fcfd5880bb81015fbf10e0ba
AnastasiyaBH <bhanastasia495@gmail.com>
2019-08-01 12:52:43 +0300

    feat: add query processing function

--- src/components/writersList/index.js
+++ src/components/writersList/index.js
@@ -6,19 +6,37 @@ import styles from './styles';
 import Card from './card';
 import info from './info';
 
-const WritersList = ({ classes: { writersList } }) => (
-  <Grid className={writersList} container justify="center" spacing={2}>
-    {Array.from({ length: 6 }, () => null).map((field, i) => (
-      <Grid item sm={4} key={i.toString() + 1}>
-        <Card info={info[i]} />
-      </Grid>
-    ))}
-  </Grid>
-);
+const UNSEARCH_KEY = -1;
+
+const WritersList = ({ classes: { writersList }, query }) => {
+  const arr = [];
+
+  info.forEach(
+    (value) => {
+      const { name, place } = value;
+      if (name.search(new RegExp(query, 'i')) !== UNSEARCH_KEY || place.search(new RegExp(query, 'i')) !== UNSEARCH_KEY) {
+        arr.push(value);
+      }
+    },
+  );
+
+  return (
+    <Grid className={writersList} container justify="center" spacing={2}>
+      {
+        Array.from({ length: arr.length }, () => null).map((field, i) => (
+          <Grid item sm={4} key={i.toString() + 1}>
+            <Card info={arr[i]} />
+          </Grid>
+        ))
+      }
+    </Grid>
+  );
+};
 
 
 WritersList.propTypes = {
   classes: PropTypes.objectOf(PropTypes.object).isRequired,
+  query: PropTypes.string.isRequired,
 };
 
 export default withStyles(styles)(WritersList);
