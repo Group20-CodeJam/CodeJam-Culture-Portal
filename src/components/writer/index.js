540c4a33767e9b8c7a12224aed8e9f03f97d1c23
Alena Chyrskaya <helen.dubinskaya@gmail.com>
2019-08-01 13:59:42 +0200

    refactor: separation writer file on component and container

---
+++ src/components/writer/index.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Timeline, TimelineItem } from 'vertical-timeline-component-for-react';
+import Carousel from 'react-images';
+import ReactMarkdown from 'react-markdown';
+import { Link } from 'gatsby';
+import Video from '../video';
+import Layout from '../layout';
+
+
+const WriterComponent = ({
+  authorsName, yearsOfLife, image, timelineContent, GalleryContent, listOfWorks, video,
+}) => (
+  <Layout>
+    <div className="writer">
+      <h1>{authorsName}</h1>
+      <p>{yearsOfLife}</p>
+      <img alt={authorsName} src={image.file.url} />
+      <p className="body-text">
+        <Timeline lineColor="#ddd">
+          {timelineContent.map(item => (
+            <TimelineItem
+              key={item.period}
+              dateText={item.period}
+              style={{ color: '#e86971' }}
+            >
+              <p>
+                {item.text}
+
+              </p>
+
+            </TimelineItem>
+          ))}
+
+        </Timeline>
+      </p>
+      <p><ReactMarkdown source={listOfWorks.listOfWorks} /></p>
+      <Carousel views={GalleryContent} />
+      <Video videoId={video} />
+
+      <Link to="/writers/">View more writers</Link>
+      <Link to="/">Back to Home</Link>
+    </div>
+
+  </Layout>
+);
+
+WriterComponent.propTypes = {
+  authorsName: PropTypes.string.isRequired,
+  yearsOfLife: PropTypes.string.isRequired,
+  image: PropTypes.objectOf(PropTypes.object).isRequired,
+  timelineContent: PropTypes.objectOf(PropTypes.object).isRequired,
+  listOfWorks: PropTypes.objectOf(PropTypes.object).isRequired,
+  GalleryContent: PropTypes.objectOf(PropTypes.object).isRequired,
+  video: PropTypes.string.isRequired,
+};
+
+export default WriterComponent;
---
+++ src/containers/writerContainer/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import WriterComponent from '../../components/writer';
+
+
+const WriterContainer = ({
+  authorsName, yearsOfLife, image, biography, listOfWorks, photoGallery, video,
+}) => {
+  const timelineContent = [];
+  biography.forEach((item) => {
+    timelineContent.push(
+      {
+        period: item.period,
+        text: item.text,
+      },
+    );
+  });
+
+  const GalleryContent = [];
+  photoGallery.forEach((item) => {
+    GalleryContent.push(
+      {
+        src: item.file.url,
+      },
+    );
+  });
+
+  return (
+    <WriterComponent
+      authorsName={authorsName}
+      yearsOfLife={yearsOfLife}
+      image={image}
+      timelineContent={timelineContent}
+      GalleryContent={GalleryContent}
+      listOfWorks={listOfWorks}
+      video={video}
+    />
+  );
+};
+
+WriterContainer.propTypes = {
+  authorsName: PropTypes.string.isRequired,
+  yearsOfLife: PropTypes.string.isRequired,
+  image: PropTypes.objectOf(PropTypes.object).isRequired,
+  biography: PropTypes.objectOf(PropTypes.object).isRequired,
+  listOfWorks: PropTypes.objectOf(PropTypes.object).isRequired,
+  photoGallery: PropTypes.objectOf(PropTypes.object).isRequired,
+  video: PropTypes.string.isRequired,
+};
+
+export default WriterContainer;
