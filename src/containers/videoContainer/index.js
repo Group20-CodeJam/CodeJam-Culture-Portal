daffea9c6facee5125db78b3a04239641467fd19
AnastasiyaBH <bhanastasia495@gmail.com>
2019-08-02 11:17:59 +0300

    style: add VideoContainer style

--- src/containers/videoContainer/index.js
+++ src/containers/videoContainer/index.js
@@ -1,10 +1,12 @@
 import React, { useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/styles';
+
 import Button from '@material-ui/core/Button';
-
 import Video from '../../components/video';
+import styles from './styles';
 
-const VideoContainer = ({ videoId }) => {
+const VideoContainer = ({ videoId, classes: { button, videoContainer } }) => {
   const [isOpen, setIsOpen] = useState('');
 
   const openModal = useCallback(
@@ -22,15 +24,27 @@ const VideoContainer = ({ videoId }) => {
   );
 
   return (
-    <div>
-      <Video videoId={videoId} openModal={openModal} onClose={closeModal} isOpen={isOpen} />
-      <Button onClick={openModal}>Open</Button>
+    <div className={videoContainer}>
+      <Video
+        videoId={videoId}
+        openModal={openModal}
+        onClose={closeModal}
+        isOpen={isOpen}
+      />
+      <Button
+        className={button}
+        onClick={openModal}
+        variant="outlined"
+      >
+        Просмотреть видео
+      </Button>
     </div>
   );
 };
 
 VideoContainer.propTypes = {
   videoId: PropTypes.string.isRequired,
+  classes: PropTypes.objectOf(PropTypes.object).isRequired,
 };
 
-export default VideoContainer;
+export default withStyles(styles)(VideoContainer);
