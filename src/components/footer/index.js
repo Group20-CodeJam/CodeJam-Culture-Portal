a6ae56033f366a0ccb999ca5c2562ff03ded27ed
Vadzim Minin <vadzim.minin@gmail.com>
2019-08-04 23:23:28 +0300

    fix: change css

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/styles';
-import styles from './styles';
+import styles from './styles.css';
 
-const Footer = ({
-  classes: { footer, text }
-}) => (
-  <footer className={footer}>
-    <p className={text}>Made by Group20 © 2019</p>
+const Footer = () => (
+  <footer className="footer">
+    <p className="text">Made by Group20 © 2019</p>
   </footer>
 );
 
-Footer.propTypes = {
-  classes: PropTypes.objectOf(PropTypes.object).isRequired,
-};
 
 export default withStyles(styles)(Footer);
